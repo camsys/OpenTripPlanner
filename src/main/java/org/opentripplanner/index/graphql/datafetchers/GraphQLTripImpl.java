f44230313d17fa15ea19b0ab5359117b784fed77
Mitchell Salvatore <msalvatore@camsys.com>
2022-06-27 15:00:19 -0400

    fix commit

--- src/main/java/org/opentripplanner/index/graphql/datafetchers/GraphQLTripImpl.java
+++ src/main/java/org/opentripplanner/index/graphql/datafetchers/GraphQLTripImpl.java
@@ -10,8 +10,6 @@ import org.onebusaway.gtfs.model.FeedInfo;
 import org.onebusaway.gtfs.model.Route;
 import org.onebusaway.gtfs.model.Stop;
 import org.onebusaway.gtfs.model.Trip;
-import org.onebusaway.gtfs.model.calendar.ServiceDate;
-import org.onebusaway.gtfs.services.calendar.CalendarService;
 import org.opentripplanner.gtfs.GtfsLibrary;
 import org.opentripplanner.index.graphql.GraphQLRequestContext;
 import org.opentripplanner.index.graphql.generated.GraphQLDataFetchers;
@@ -124,28 +122,18 @@ public class GraphQLTripImpl implements GraphQLDataFetchers.GraphQLTrip {
 	    	return GraphQLBikesAllowed.values()[t.getBikesAllowed()].name();
 	    };
 	}
-
+	
 	@Override
 	public DataFetcher<Iterable<Object>> alerts() {
 		return environment -> {
-			Trip t = environment.getSource();
+	    	Trip t = environment.getSource();
 
 			return getRouter(environment).graph.getAlertPatches()
 					.filter(s -> s.getTrip() != null ? s.getTrip().equals(t.getId()) : false)
 					.collect(Collectors.toList());
-		};
+		};	
 	}
-
-	@Override
-	public DataFetcher<String> tripDateTime() {
-		return environment -> {
-			Trip t = environment.getSource();
-			CalendarService calendarService = getGraphIndex(environment).graph.getCalendarService();
-			Set<ServiceDate> serviceDates = calendarService.getServiceDatesForServiceId(t.getServiceId());
-			return serviceDates.iterator().next().getAsString();
-		};
-	}
-
+	
 	private Router getRouter(DataFetchingEnvironment environment) {
 		return environment.<GraphQLRequestContext>getContext().getRouter();
 	}
