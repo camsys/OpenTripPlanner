b30e4a8785ef6a092909ccae944f2f04535c5b09
carabalb <lennycaraballo@gmail.com>
2023-06-12 17:14:35 -0400

    [MNDOT-125] More fixes

--- src/main/java/org/opentripplanner/routing/algorithm/raptor/transit/FlexAccessEgressAdapter.java
+++ src/main/java/org/opentripplanner/routing/algorithm/raptor/transit/FlexAccessEgressAdapter.java
@@ -1,9 +1,13 @@
 package org.opentripplanner.routing.algorithm.raptor.transit;
 
 import org.opentripplanner.ext.flex.FlexAccessEgress;
+import org.opentripplanner.ext.flex.FlexIndex;
+import org.opentripplanner.ext.flex.trip.FlexTrip;
 import org.opentripplanner.transit.raptor.api.transit.RaptorTransfer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.opentripplanner.model.StopPattern.PICKDROP_NONE;
 
 /**
  * This class is used to adapt the FlexAccessEgress into a time-dependent multi-leg AccessEgress.
@@ -14,13 +18,22 @@ public class FlexAccessEgressAdapter extends AccessEgress implements RaptorTrans
 
   private final FlexAccessEgress flexAccessEgress;
 
+  private final boolean allowPickupDropoff;
+
   public FlexAccessEgressAdapter(
-          FlexAccessEgress flexAccessEgress, boolean isEgress, StopIndexForRaptor stopIndex
+          FlexAccessEgress flexAccessEgress, FlexIndex flexIndex, boolean isEgress, StopIndexForRaptor stopIndex
   ) {
     super(
         stopIndex.indexByStop.get(flexAccessEgress.stop),
         isEgress ? flexAccessEgress.lastState.reverse() : flexAccessEgress.lastState
     );
+
+    FlexTrip trip = flexAccessEgress.trip;
+
+    boolean allowPickup = flexIndex.hasStopThatAllowsPickup(trip,trip.getStopTime(flexAccessEgress.fromStopIndex).stop);
+    boolean allowDropoff = flexIndex.hasStopThatAllowsDropoff(trip,trip.getStopTime(flexAccessEgress.toStopIndex).stop);
+    this.allowPickupDropoff = allowDropoff && allowPickup;
+
 
     this.flexAccessEgress = flexAccessEgress;
   }
@@ -44,6 +57,11 @@ public class FlexAccessEgressAdapter extends AccessEgress implements RaptorTrans
   @Override
   public boolean stopReachedOnBoard() {
     return flexAccessEgress.directToStop;
+  }
+
+  @Override
+  public boolean allowPickupDropoff() {
+    return allowPickupDropoff;
   }
 
   @Override
