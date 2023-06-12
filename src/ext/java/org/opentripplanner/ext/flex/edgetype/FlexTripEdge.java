0c9863975ab47b3ee751e8be1f0c43afa3bc75e2
carabalb <lennycaraballo@gmail.com>
2023-06-12 19:32:09 -0400

    Seriously the last fix

--- src/ext/java/org/opentripplanner/ext/flex/edgetype/FlexTripEdge.java
+++ src/ext/java/org/opentripplanner/ext/flex/edgetype/FlexTripEdge.java
@@ -117,7 +117,7 @@ public class FlexTripEdge extends Edge {
 	    if(wait < 0)
 	    	return null; // missed it
     }
-    else if(getFlexTrip() instanceof UnscheduledTrip && (allowPickup || !allowDropoff)){
+    else if(getFlexTrip() instanceof UnscheduledTrip && (!allowPickup || !allowDropoff)){
         return null;
     }
     
